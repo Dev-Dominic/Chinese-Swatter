1574a29d320e3f37f648f806436fad4090f17003
Dominic Henry <dominichenrywork@hotmail.com>
2020-04-16 20:27:06 -0500

    Update  running environment
    Phased out pipenv virtual environment setup in favor of using
    python's built in virtual environment venv. Thus an addition of a requirements.txt
    file.
    
    .gitignore was updated to reflect a python and flask application ignore file.
    
    Minor:
    
    Delete entry point file 'run.py' to be done in favor of using the app module
    as the entry point for the file.

--- app/__init__.py
+++ app/__init__.py
@@ -2,5 +2,7 @@
 from flask import Flask
 app = Flask(__name__)
 
+print("hello world")
+
 from app import views
 
--- run.py
+++
@@ -1,8 +0,0 @@
-#! /usr/bin/env python 
-# Flask run script
-
-from app import app
-
-if __name__ == "__main__": 
-    # Parameters: host, port, debug, options
-    app.run("localhost", 8080, True)
